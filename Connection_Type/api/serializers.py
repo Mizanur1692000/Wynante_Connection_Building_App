a335d989c92c5ad2ef283a7c4db43031e78ce604
Mizanur1692000 <mizanur.rahman.1cse@gmail.com>
2026-01-11 04:53:35 +0600

    Completed the conversation part so far

--- Connection_Type/api/serializers.py
+++ Connection_Type/api/serializers.py
@@ -6,10 +6,3 @@ class ConnectionDistributionSerializer(serializers.Serializer):
     distribution = serializers.DictField(child=serializers.IntegerField(min_value=0, max_value=100))
     pair_key = serializers.CharField()
     message_count = serializers.IntegerField(min_value=0)
-
-
-class ConnectionDistributionSerializer(serializers.Serializer):
-    highest_connection_type = serializers.ChoiceField(choices=CONNECTION_TYPE_KEYS)
-    distribution = serializers.DictField(child=serializers.IntegerField(min_value=0, max_value=100))
-    pair_key = serializers.CharField()
-    message_count = serializers.IntegerField(min_value=0)
